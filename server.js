e02d68c48a98bcffc69e8ead47649a64e6c4d634
Antonio Castiglione <antonio@minervawebdevelopment.com>
2020-03-01 14:11:05 -0500

    fixed git pathing and updated live build in server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
@@ -22,7 +22,13 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/transactions", transactions);
 
 // Must be below API routes
-if()
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
